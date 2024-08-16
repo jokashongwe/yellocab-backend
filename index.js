f151b8c178cf9c7c67f4c74fba5ccd4b0f90785e
Jonathan Kashongwe <jokashongwe@gmail.com>
2024-08-16 05:50:34 +0100

    Add Port

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const mongoDbConnectionString = require('./config/mongodb')
-const userRouter = require('./routes/user')
-const authRouter = require('./routes/auth')
-const authMiddleware = require('./middleware/auth')
-const errorMiddleware = require('./middleware/error')
-const PORT = 4000;
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const mongoDbConnectionString = require("./config/mongodb");
+const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+const authMiddleware = require("./middleware/auth");
+const errorMiddleware = require("./middleware/error");
+require("dotenv").config();
+const PORT = process.env.SERVER_PORT || 4000;
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/auth', authRouter)
-app.use('*', authMiddleware)
-app.use('/users', userRouter)
-app.use(errorMiddleware)
+app.use("/auth", authRouter);
+app.use("*", authMiddleware);
+app.use("/users", userRouter);
+app.use(errorMiddleware);
 
-mongoose.
-    connect(mongoDbConnectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    })
-    .then(result => {
-        console.log('Coneected to MongoDB')
-        app.listen(PORT, '0.0.0.0' ,() => {
-            console.log("Server is listening on PORT: " + PORT)
-        })
-    })
-    .catch(err => {
-        console.log(err)
-    })
-
+mongoose
+  .connect(mongoDbConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("Coneected to MongoDB");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("Server is listening on PORT: " + PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
